a6006a0e41817f88d83b2252c40b04a7ba08072d
gv7 <guzao4880@gmail.com>
2025-02-10 10:44:16 -0300

    .

---
+++ aula01/src/ConceitosBasicos/variaveis.java
@@ -0,0 +1,11 @@
+package ConceitosBasicos;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.print("Olá mundo!");
+
+	}
+
+}
