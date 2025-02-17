183906fc676b28ddeb8214a04f209d46fe0428ac
gv137 <145298212+gv137@users.noreply.github.com>
2025-02-17 10:57:57 -0300

    desafio

--- aula2Variaveis/src/desafios/desafio2.java
+++ aula2Variaveis/src/desafios/desafio2.java
@@ -1,9 +1,25 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class desafio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// calculadora de juros compostos
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("insira o capital inicial: ");
+		double capInicial = sc.nextDouble(); 
+		System.out.println("insira a taxa de juros: ");
+		double txJuros = sc.nextDouble(); 
+		System.out.println("insira o número de meses: ");
+		double nMeses = sc.nextDouble(); 
+		
+		double mFinal = capInicial * (1 + txJuros)^nMeses;
+		
+		
+		sc.close();
 
 	}
 
