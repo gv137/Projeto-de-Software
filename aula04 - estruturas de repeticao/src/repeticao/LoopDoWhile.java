fa4111074fe4a1a7bd66f858eaf671e7b9c0db83
gv137 <145298212+gv137@users.noreply.github.com>
2025-03-10 09:06:59 -0300

    .

---
+++ aula04 - estruturas de repeticao/src/repeticao/LoopDoWhile.java
@@ -0,0 +1,25 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class LoopDoWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int maquina = (int)(10 * Math.random());
+		int usuario;
+		
+		do {
+			System.out.println("informe um número entre 0 e 9: ");
+			usuario = sc.nextInt();
+		} while(maquina != usuario);
+		
+		System.out.println("Parabéns, você venceu!!!");
+		
+		sc.close();
+		
+
+	}
+
+}
