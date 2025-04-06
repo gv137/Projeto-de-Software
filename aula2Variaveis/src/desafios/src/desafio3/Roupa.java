b5d55334c8bc4dea3e72c52aa45cdaffd87f4925
gv7 <guzao4880@gmail.com>
2025-04-06 19:44:12 -0300

    .

--- aula2Variaveis/src/desafios/src/desafio3/Roupa.java
+++ aula2Variaveis/src/desafios/src/desafio3/Roupa.java
@@ -1,43 +1,80 @@
 package desafio3;
 
 public class Roupa {
-	
-	
-    String marca;
-    String tipo;
-    String tamanho;
-    int quantidade;
-    double valor;
-    
-	
-    
-    @Override
-	public String toString() {
-		return "Roupa [marca=" + marca + ", tipo=" + tipo + ", tamanho=" + tamanho + ", quantidade=" + quantidade
-				+ ", valor=" + valor + "]";
-	}
+    private String marca;
+    private String tipo;
+    private String tamanho;
+    private int quantidade;
+    private double valor;
 
-
-	public double calcularValorTotal() {
-        return quantidade * valor;
+    
+    public Roupa(String marca, String tipo, String tamanho, int quantidade, double valor) {
+        this.marca = marca;
+        this.tipo = tipo;
+        this.tamanho = tamanho;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
 
    
+    public double calcularValorTotal() {
+        return quantidade * valor;
+    }
+
+    
     public void adicionarEstoque(int quantidade) {
         if (quantidade > 0) {
             this.quantidade += quantidade;
         }
     }
 
-   
+    
     public void removerEstoque(int quantidade) {
         if (quantidade > 0 && this.quantidade >= quantidade) {
             this.quantidade -= quantidade;
         } else {
             System.out.println("Quantidade insuficiente em estoque.");
         }
-    
-    
-    
+    }
+
+    // Getters e Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
     }
 }
